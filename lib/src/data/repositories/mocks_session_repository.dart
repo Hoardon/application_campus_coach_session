6b73270740f8a736aeaa8fdf97e913b14468b046
vlaurent <valentin.laurent.dev@gmail.com>
2026-02-19 16:43:02 +0100

    refacto: refacto mock repository to add cache

--- lib/src/data/repositories/mocks_session_repository.dart
+++ lib/src/data/repositories/mocks_session_repository.dart
@@ -5,19 +5,27 @@ import 'package:application_campus_coach_session/src/domain/entities/session.dar
 
 class MocksSessionRepository extends SessionRepository {
   final SessionLocalDataSource dataSource;
+  List<Session>? _cache;
 
   MocksSessionRepository({required this.dataSource});
 
   @override
   Future<List<Session>> getSessions() async {
+    if (_cache != null) return _cache!;
+
     final models = await dataSource.getSessions();
-    return models.map((e) => e.toEntity()).toList();
+    final sessionsList = models.map((e) => e.toEntity()).toList();
+    _cache = sessionsList;
+    return sessionsList;
   }
 
   @override
   Future<Session> getSessionDetailsById({required String id}) async {
-    final models = await dataSource.getSessions();
-    final model = models.firstWhere((element) => element.id == id);
-    return model.toEntity();
+    if (_cache != null) {
+      return _cache!.firstWhere((element) => element.id == id);
+    }
+
+    final sessionsList = await getSessions();
+    return sessionsList.firstWhere((element) => element.id == id);
   }
 }
