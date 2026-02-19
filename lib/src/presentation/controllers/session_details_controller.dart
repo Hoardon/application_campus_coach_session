f8b500341deb18248db9383b716aa9322f1d84e4
vlaurent <valentin.laurent.dev@gmail.com>
2026-02-19 17:53:53 +0100

    feat: Add sessions && session details controllers

---
+++ lib/src/presentation/controllers/session_details_controller.dart
@@ -0,0 +1,21 @@
+import 'package:application_campus_coach_session/src/core/presentation/ui_state.dart';
+import 'package:application_campus_coach_session/src/domain/entities/session.dart';
+import 'package:application_campus_coach_session/src/domain/usecases/get_session_details_by_id.dart';
+import 'package:get/get.dart';
+
+class SessionDetailsController extends GetxController {
+  final GetSessionDetailsById getSessionDetailsById;
+
+  SessionDetailsController({required this.getSessionDetailsById});
+
+  final state = Rx<UiState<Session>>(const UiLoading());
+
+  Future<void> loadSelectedSession({required String id}) async {
+    state.value = const UiLoading();
+    try {
+      state.value = UiSuccess(await getSessionDetailsById(id: id));
+    } catch (e) {
+      state.value = UiError(e.toString());
+    }
+  }
+}
---
+++ lib/src/presentation/controllers/sessions_controller.dart
@@ -0,0 +1,21 @@
+import 'package:application_campus_coach_session/src/core/presentation/ui_state.dart';
+import 'package:application_campus_coach_session/src/domain/entities/session.dart';
+import 'package:application_campus_coach_session/src/domain/usecases/get_sessions.dart';
+import 'package:get/get.dart';
+
+class SessionsController extends GetxController {
+  final GetSessions getSessionsUseCase;
+
+  SessionsController({required this.getSessionsUseCase});
+
+  final state = Rx<UiState<List<Session>>>(const UiLoading());
+
+  Future<void> loadSessions() async {
+    state.value = const UiLoading();
+    try {
+      state.value = UiSuccess(await getSessionsUseCase());
+    } catch (e) {
+      state.value = UiError(e.toString());
+    }
+  }
+}
