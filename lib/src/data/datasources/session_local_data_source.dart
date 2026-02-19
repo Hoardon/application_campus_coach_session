676d62162278e8691df75bad8bff9678c3efc020
vlaurent <valentin.laurent.dev@gmail.com>
2026-02-19 16:15:06 +0100

    feat: Add fetching mock local data source class

---
+++ lib/src/data/datasources/session_local_data_source.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'package:application_campus_coach_session/src/data/models/session_model.dart';
+import 'package:flutter/services.dart';
+
+abstract class SessionLocalDataSource {
+  Future<List<SessionModel>> getSessions();
+}
+
+class SessionLocalDataSourceImpl implements SessionLocalDataSource {
+  @override
+  Future<List<SessionModel>> getSessions() async {
+    final String jsonString = await rootBundle.loadString(
+      'assets/mocks/sessions_mocks.json',
+    );
+
+    final List<dynamic> jsonList = json.decode(jsonString);
+
+    return jsonList.map((jsonItem) => SessionModel.fromJson(jsonItem)).toList();
+  }
+}
